d588f9381142c21529226381c54d49496e053687
KC-14 <1052048733@qq.com>
2018-12-21 17:34:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jyj/myapplication/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.jyj.myapplication
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.support.annotation.RequiresApi
+import android.util.Log
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils
+import com.davemorrissey.labs.subscaleview.ImageSource
+import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+
+    val key: String = "hello  ll llll mm nn   nnnnn"
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkPermissions()
+        setContentView(R.layout.activity_main)
+        btn.setOnClickListener {
+            var createBitmap = signView.bitmap
+            var mergeBitmap = mergeBitmap(createBitmap)
+            imageView.setImage(ImageSource.bitmap(mergeBitmap))
+            downLoadImg(mergeBitmap)
+        }
+        btnClear.setOnClickListener {
+            signView.clear()
+        }
+    }
+
+    fun mergeBitmap(bmp2: Bitmap): Bitmap {
+        var bmp1 = BitmapFactory.decodeResource(resources, R.mipmap.img1)
+        var resultBmp = Bitmap.createBitmap(bmp1.width, bmp1.height, Bitmap.Config.RGB_565)
+        var canves = Canvas(resultBmp)
+
+        var rect1 = Rect(0, 0, bmp1.width, bmp1.height)
+        canves.drawBitmap(bmp1, null, rect1, null)
+
+        val bitmapPool = Glide.get(this).bitmapPool
+        val bitmap1 = TransformationUtils.fitCenter(bitmapPool, bmp2, 400, 100)
+        Log.e("AAA", "getBitmap: bitmap后~~~  getWidth   " + bitmap1.width)
+        Log.e("AAA", "getBitmap: bitmap后~~~  getHeight   " + bitmap1.height)
+        canves.drawBitmap(bitmap1, 928f,4345f,null)
+        return resultBmp
+    }
+
+    /**
+     * 检查权限
+     */
+    @AfterPermissionGranted(100)
+    fun checkPermissions() {
+        if (!EasyPermissions.hasPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            EasyPermissions.requestPermissions(this,"权限",100, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun downLoadImg(bitmap: Bitmap) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            if (EasyPermissions.hasPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                saveBitmap(bitmap)
+            }
+        } else {
+            saveBitmap(bitmap)
+        }
+    }
+
+    fun saveBitmap(bitmap: Bitmap) {
+        Thread(Runnable {
+            try {
+                val name = SimpleDateFormat("yyyyMMddHHmmss").format(Date()) + ".jpg"
+                val path = File(Environment.getExternalStorageDirectory().toString() + "/DCIM", "images")
+                val output = File(path, name)
+                val imagePath = output.getPath()
+                if (!output.getParentFile().exists()) output.getParentFile().mkdirs()
+                val f = File(imagePath)
+
+                val os = FileOutputStream(f)
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+                os.close()
+                val uri = Uri.fromFile(f)
+                sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri))
+                runOnUiThread { Toast.makeText(this, "下载成功，请到相册中查看", Toast.LENGTH_SHORT) }
+            } catch (e: Exception) {
+
+            }
+        }).start()
+    }
+}
---
+++ app/src/main/java/com/example/jyj/myapplication/SignatureView.java
@@ -0,0 +1,101 @@
+package com.example.jyj.myapplication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SignatureView extends View {
+    private static final String TAG = "SignatureView";
+    Paint paint = new Paint();  //定义画笔
+
+    private Canvas mCanvas;     //定义画布
+    private Bitmap mBitmap;
+
+    private float startX;       //手写起点
+    private float startY;
+    private List<Float> xList=new ArrayList<>();
+    private List<Float> yList=new ArrayList<>();
+
+    public SignatureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mCanvas = new Canvas();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(16);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeMiter(90);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas.setBitmap(mBitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = event.getX();//将手接触屏幕时的位置设为起点
+                startY = event.getY();
+                xList.add(startX);
+                yList.add(startY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mCanvas.drawLine(startX, startY,
+                        event.getX(), event.getY(), paint);//画起点到移动点间的线
+                postInvalidate();//视图刷新
+                startX = event.getX();//重改起点，这样才能绘制出随着手移动的曲线来
+                startY = event.getY();
+                xList.add(startX);
+                yList.add(startY);
+                break;
+            case MotionEvent.ACTION_UP:
+                mCanvas.drawLine(startX, startY, event.getX(), event.getY(), paint);
+                postInvalidate();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+    }
+
+    public void clear() {
+        if (mBitmap!=null){
+            mBitmap.eraseColor(Color.TRANSPARENT);
+            invalidate();
+        }
+    }
+
+        public Bitmap getBitmap(){
+            float xMax=Collections.max(xList);
+            float xMin=Collections.min(xList);
+            float yMax=Collections.max(yList);
+            float yMin=Collections.min(yList);
+            int width= (int) (xMax-xMin);
+            int height= (int) (yMax-yMin);
+            Log.e(TAG, "getBitmap: bitmap--0 getWidth   "+mBitmap.getWidth());
+            Log.e(TAG, "getBitmap: bitmap--0  getHeight   "+mBitmap.getHeight());
+            Bitmap bitmap = Bitmap.createBitmap(mBitmap,(int)xMin,(int)yMin,width,height);
+            Log.e(TAG, "getBitmap: bitmap原 getWidth   "+bitmap.getWidth());
+            Log.e(TAG, "getBitmap: bitmap原 getHeight   "+bitmap.getHeight());
+            return bitmap;
+    }
+}
